0d55ba723d7a042d904105af543cc23c3e778b91
Jesse Greathouse <jessegreathouse@gmail.com>
2023-10-22 00:19:32 +0000

    fixed errors

--- bin/modules/Mcol/Utility.pm
+++ bin/modules/Mcol/Utility.pm
@@ -103,20 +103,20 @@ sub generate_rand_str {
 sub splash {
   print (''."\n");
   print ('+--------------------------------------------------------------------------------------+'."\n");
-  print ('| Thank you for choosing mcol                                                    |'."\n");
+  print ('| Thank you for choosing mcol                                                          |'."\n");
   print ('+--------------------------------------------------------------------------------------+'."\n");
   print ('| Copyright (c) 2023 Jesse Greathouse (https://github.com/jesse-greathouse/mcol)       |'."\n");
   print ('+--------------------------------------------------------------------------------------+'."\n");
-  print ('| mcol is free software: you can redistribute it and/or modify it under the      |'."\n");
+  print ('| mcol is free software: you can redistribute it and/or modify it under the            |'."\n");
   print ('| terms of thethe Free Software Foundation, either version 3 of the License, or GNU    |'."\n");
   print ('| General Public License as published by (at your option) any later version.           |'."\n");
   print ('|                                                                                      |'."\n");
-  print ('| mcol is distributed in the hope that it will be useful, but WITHOUT ANY        |'."\n");
+  print ('| mcol is distributed in the hope that it will be useful, but WITHOUT ANY              |'."\n");
   print ('| WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A      |'."\n");
   print ('| PARTICULAR PURPOSE.  See the GNU General Public License for more details.            |'."\n");
   print ('|                                                                                      |'."\n");
   print ('| You should have received a copy of the GNU General Public License along with         |'."\n");
-  print ('| mcol. If not, see <http://www.gnu.org/licenses/>.                              |'."\n");
+  print ('| mcol. If not, see <http://www.gnu.org/licenses/>.                                    |'."\n");
   print ('+--------------------------------------------------------------------------------------+'."\n");
   print ('| Author: Jesse Greathouse <jesseg@wheelpros.com>                                      |'."\n");
   print ('+--------------------------------------------------------------------------------------+'."\n");
