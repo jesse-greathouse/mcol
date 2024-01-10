899f572fb201451a8d2a88ef76a8469d8ead7e0d
Jesse Greathouse <jessegreathouse@gmail.com>
2024-01-10 08:53:06 -0700

    fixed modes

--- src/app/Dcc/Client.php
+++ src/app/Dcc/Client.php
@@ -3,10 +3,13 @@
 namespace App\Dcc;
 
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Process;
 
 use App\Exceptions\IllegalPacketException,
+    App\Models\Bot,
     App\Models\Download,
-    App\Models\Packet;
+    App\Models\Packet,
+    App\Models\Network;
 
 class Client
 {
@@ -40,9 +43,10 @@ class Client
      * @param string $fileName
      * @param int|null $fileSize
      * @param string|null $botId
+     * @param bool $resume
      * @return void
      */
-    public function open(string $host, string $port, string $fileName, int $fileSize = null, $botId = null): void
+    public function open(string $host, string $port, string $fileName, int $fileSize = null, $botId = null, bool $resume = false): void
     {
         $bytes = 0;
         $downloadDir = env('DOWNLOAD_DIR', '/var/download');
@@ -80,13 +84,44 @@ class Client
                 }
             }
 
-            $meta = stream_get_meta_data($file);
-            if (isset($meta['uri'])) {
-                $download->progress_bytes = filesize($meta['uri']);
+            // If expected file size wasn't sent as a parameter, 
+            // it's impossible to know how large the file should be.
+            if (null === $fileSize) { 
+                $fileSize = $download->progress_bytes;
             }
 
-            $download->status = Download::STATUS_COMPLETED;
-            $download->save();
+            $meta = stream_get_meta_data($file);
+
+            if (isset($meta['uri'])) {
+                $bytesDownloaded = filesize($meta['uri']);
+                if ((integer) $bytesDownloaded === (integer) $fileSize) {
+                    $download->status = Download::STATUS_COMPLETED;
+                    $download->progress_bytes = null;
+                    $download->file_size_bytes = null;
+                    $download->save();
+                }
+            } else {
+                $dir = env('DIR', '/usr');
+                $src = env('SRC', '/usr/src');
+                $bin = "$dir/bin";
+                $network = Network::where('id', $packet->network_id)->first();
+                $bot = Bot::where('id', $packet->bot_id)->first();
+                $command = "XDCC SEND $packet->number";
+                $this->console->info("Download of stream: \"$fileName\" closed prematurely.");
+
+                if (null !== $network && null !== $bot) {
+                    $this->console->info("Queueing for resume (at $download->progress_bytes of $fileSize )");
+
+                    $console = $this->console;
+                    Process::path($src)->start("$bin/php artisan mcol:chat $network->name '$bot->nick' '$command'", function (string $type, string $output) use ($console, $download) {
+                        $download->queued_status = 0;
+                        $download->save();
+                        $console->info("Command $type output: $output");
+                    });
+                } else {
+                    $this->console->info("Unable to Queue for resume because of incomplete network or bot data.");
+                }
+            }
 
             fclose($file);
             fclose($fp);
@@ -123,5 +158,16 @@ class Client
             [ 'file_uri' => $uri, 'packet_id' => $packetId ],
             [ 'status' => Download::STATUS_INCOMPLETE, 'file_size_bytes' => $fileSize, 'progress_bytes' => $bytes,  ]
         );
+    }
+
+    /**
+     * Removes all Non-numeric characters from a string.
+     *
+     * @param string $txtStr
+     * @return string
+     */
+    public function clnNumericStr(string $txtStr): string
+    {
+        return preg_replace("/[^0-9]/", "", $txtStr);
     }
 }
