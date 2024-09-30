df40f29f7f4c9f1c6df38c8a0488cf2c94a36827
Jesse Greathouse <jessegreathouse@gmail.com>
2024-09-30 21:41:07 +0000

    Added languages support in the browse UI

--- src/resources/js/Components/LanguageFilter.vue
+++ src/resources/js/Components/LanguageFilter.vue
@@ -1,15 +1,15 @@
 <template>
   <div class="flex w-auto items-center m-3">
     <label class="inline-flex items-center cursor-pointer">
-      <input v-checked="exclude" @input="$emit('update:excludelanguage', $event.target.checked)" type="checkbox" value="" class="sr-only peer">
+      <input v-bind:checked="exclude" @input="$emit('update:excludeLanguage', $event.target.checked)" type="checkbox" value="" class="sr-only peer">
       <div class="relative w-11 h-6 bg-gray-200 rounded-full peer peer-focus:ring-4 peer-focus:ring-red-300 dark:peer-focus:ring-red-800 dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-red-600"></div>
     </label>
   </div>
 
   <ul class="items-center w-auto text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg sm:flex dark:bg-gray-700 dark:border-gray-600 dark:text-white">
-      <li v-for="language in languages" class="w-full border-b border-gray-200 sm:border-b-0 sm:border-r dark:border-gray-600">
+      <li v-for="language in languages" ref="items" class="w-full border-b border-gray-200 sm:border-b-0 sm:border-r dark:border-gray-600">
           <div class="flex items-center ps-2">
-              <input v-checked="languageSelected(language)" v-bind:id="language" @input="$emit('update:languages', language, $event.target.checked)" type="checkbox" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500">
+              <input :ref="`${language}_box`" v-bind:checked="languageSelected(language)" v-bind:id="language" @input="$emit('update:languages', language, $event.target.checked)" type="checkbox" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500">
               <label for="{{ language }}" class="w-full py-3 ms-1 mr-2 text-sm font-medium text-gray-900 dark:text-gray-300 capitalize">{{ language }}</label>
           </div>
       </li>
@@ -17,23 +17,22 @@
 </template>
   
 <script>
+import { ref } from 'vue'
 import _ from 'lodash';
 
 export default {
   props: {
     exclude: Boolean,
-    selected: Array,
+    in_language: Array,
+    out_language: Array,
     languages: Array,
   },
   methods: {
     languageSelected(language) {
-      if (_.isSet(this.selected)) {
-        return (0 <= this.selected.indexOf(language)) ? true : false;
-      } else {
-        return false;
-      }
-    }
+      const set = (this.exclude) ? 'out_language' : 'in_language'
+      return (0 <= this[set].indexOf(language)) ? true : false;
+    },
   },
-  emits: ['update:languages'],
-  }
+  emits: ['update:languages', 'update:excludeLanguage'],
+}
 </script>
