1d06708aa85b0198d57093324cab3c67970fbb62
Jesse Greathouse <jessegreathouse@gmail.com>
2024-10-22 19:01:48 +0000

    Api routes for working with Media Store Files * Added MediaStore class for querying files and directories inside the Media Stores. * Added MediaStore Resource for forming API responses. * Added MediaStore Request for validating API requests. * Added form validation for MediaStore Requests. * Added API Routes for querying MediaStore

---
+++ src/app/Http/Resources/MediaStoreResource.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Carbon;
+
+class MediaStoreResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        $linkTarget = ($this->isLink()) ? $this->getLinkTarget() : null;
+        $uri = $this->getPath() . DIRECTORY_SEPARATOR . $this->getFilename();
+        return [
+            'uri'               => $uri,
+            'basename'          => $this->getBasename(),
+            'modified'          => Carbon::createFromTimestamp($this->getMTime())->toDateTimeString(),
+            'changed'           => Carbon::createFromTimestamp($this->getCTime())->toDateTimeString(),
+            'owner'             => $this->getOwner(),
+            'group'             => $this->getGroup(),
+            'inode'             => $this->getInode(),
+            'linkTarget'        => $linkTarget,
+            'path'              => $this->getPath(),
+            'path_name'         => $this->getPathName(),
+            'perms'             => $this->getPerms(),
+            'realpath'          => $this->getRealPath(),
+            'size'              => $this->getSize(),
+            'type'              => $this->getType(),
+            'is_dir'            => $this->isDir(),
+            'is_executable'     => $this->isExecutable(),
+            'is_file'           => $this->isFile(),
+            'is_link'           => $this->isLink(),
+            'is_readable'       => $this->isReadable(),
+            'is_writeable'      => $this->isWritable(),
+        ];
+    }
+}
